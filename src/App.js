cc50a42a80297e2a0b96cfd3eacd8d7231da1186
liba-ny <94902436+liba-ny@users.noreply.github.com>
2023-01-17 14:56:54 +0100

    Add stretches

--- src/App.js
+++ src/App.js
@@ -3,18 +3,44 @@ import ReactDOM from 'react-dom';
 //import './App.css';
 import style from './styles.css';
 import randomHex from 'random-hex';
+import randomColor from 'randomcolor';
 
 export default function App() {
   const random = randomHex.generate();
   const [color, setColor] = useState('');
-  // const [colorText, setColorText] = useState('');
+  const [colorChosen, setColorChosen] = useState('');
+  const [lightChosen, setLightColor] = useState('');
+
   return (
     <div className="items">
       <h1> React Random Color Generator</h1>
+      <h3>
+        <input
+          placeholder="What Color?"
+          value={colorChosen}
+          onChange={(event) => {
+            setColorChosen(event.currentTarget.value);
+          }}
+        />
+      </h3>
+      <h3>
+        <input
+          placeholder="Light or Dark?"
+          value={lightChosen}
+          onChange={(event) => {
+            setLightColor(event.currentTarget.value);
+          }}
+        />
+      </h3>
       <button
         className="btn"
         onClick={() => {
-          setColor(random);
+          setColor(
+            randomColor({
+              luminosity: lightChosen,
+              hue: colorChosen,
+            }),
+          );
         }}
       >
         Generate
@@ -22,6 +48,7 @@ export default function App() {
       <div
         style={{
           backgroundColor: color,
+          borderRadius: '6px',
         }}
       >
         <h2 className="outputcolor"> Generated Color:{color}</h2>
